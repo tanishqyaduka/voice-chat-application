6e01b7dc5d1d5a2d0ab0b070efe41ffa1f0d4764
Tanishq Yaduka <114014875+tanishqyaduka@users.noreply.github.com>
2023-08-03 16:11:13 +0530

    Add files via upload

---
+++ server2.py
@@ -0,0 +1,38 @@
+import socket
+import cv2
+import pickle
+import struct
+import time
+import pyshine as ps
+
+mode = 'send'
+name = 'SERVER TRANSMITTING AUDIO'
+audio, context = ps.audioCapture(mode=mode)
+# ps.showPlot(context,name)
+
+# Socket Create
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_ip = '192.168.208.161'
+port = 8080
+backlog = 5
+socket_address = (host_ip, port)
+print('STARTING SERVER AT', socket_address, '...')
+server_socket.bind(socket_address)
+server_socket.listen(backlog)
+
+while True:
+    client_socket, addr = server_socket.accept()
+    print('GOT CONNECTION FROM:', addr)
+    if client_socket:
+
+        while (True):
+            frame = audio.get()
+
+            a = pickle.dumps(frame)
+            message = struct.pack("Q", len(a))+a
+            client_socket.sendall(message)
+
+    else:
+        break
+
+client_socket.close()
